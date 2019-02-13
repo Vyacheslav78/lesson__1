e74e02d90d98bc3f08642a1bf55a85c17458b456
Зипа Вячеслав Сергеевич <v.zipa@Vostokhimvolokno.local>
2019-02-13 12:27:23 +0300

    Add project files.

---
+++ lesson_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  оздать новый консольный проект, создать в нём переменные всех базовых типов, присвоить им значения
+
+             *   
+             */
+            byte a;
+            a = 1;
+
+            sbyte a1;
+            a1 = -1;
+
+            short b;
+            b = -1;
+
+            ushort b1;
+            b1 = -1;
+
+            int c;
+            c = -1;
+
+            uint c1;
+            c1 = 1;
+
+            long d;
+            d = -1;
+
+            ulong d1;
+            d1 = 1;
+
+            float i;
+            i = 1;
+
+            double f;
+            f = 1;
+
+            decimal g;
+            g = 1;
+
+            string h;
+            h = "String";
+
+            bool i;
+            i = true;
+
+
+                
+        }
+    }
+}
